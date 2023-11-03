50c8fee061d03d835b7b99091945218188bf8e35
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-11-03 01:31:39 -0400

    Added basic flashlight controller action

--- app/src/main/java/com/example/flashlightcontroller/MainActivity.kt
+++ app/src/main/java/com/example/flashlightcontroller/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.flashlightcontroller
 
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.flashlightcontroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraManager: CameraManager
+    private lateinit var cameraId: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
+
     }
+
+    private fun setListeners() {
+
+        binding.swFlashlightController.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            updateFlashlight(isChecked)
+
+        }
+
+    }
+
+    private fun updateFlashlight(isChecked: Boolean) {
+
+        try {
+            cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
+            cameraId = cameraManager.cameraIdList[0]
+            cameraManager.setTorchMode(cameraId, isChecked)
+            binding.tvFlashlightStatus.text = if (isChecked) "ON" else "OFF"
+        } catch (e: CameraAccessException) {
+
+        } finally {
+            Log.d("Try", "Tried")
+        }
+
+    }
+
 }
